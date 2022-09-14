ce33115805be519b4d04010d8fa91d60e0fc3310
Paulo Sergio <pauloserg861@gmail.com>
2022-09-14 20:42:26 -0300

    criando pasta pages / components / style

---
+++ src/components/public-components/Header.tsx
@@ -0,0 +1,11 @@
+import style from '../../styles/Header.module.css'
+
+const Header = () => {
+  return(
+    <header className={style.header}>
+      Header da página
+    </header>
+  )
+}
+
+export default Header
---
+++ src/pages/public-pages/Home.tsx
@@ -0,0 +1,12 @@
+import Header from "../../components/public-components/Header";
+
+const Home = () => {
+  return(
+    <>
+      <Header />
+      Hello React Ts
+    </>
+  );
+}
+
+export default Home;
